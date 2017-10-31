8c60a3d356bd461f11123cb25a93ed7dddc53cd9
tony <zhatn2016@gmail.com>
2017-10-31 15:23:28 +0800

    stream

--- http/streamofhttp.js
+++ http/streamofhttp.js
@@ -1,24 +1,50 @@
 const http = require('http')
 
+// request is a instance of IncomingMessage
+// response is a instance of ServerReponse
+
 const server = http.createServer((req, res) => {
-  let body = ''
+  // let { method, url, headers } = req
+  // let body = ''
   // buffer object will be received
-  req.setEncoding('utf-8')
-  req.on('data', (chunk) => {
-    body += chunk
-    console.log(chunk)
+  // req.setEncoding('utf-8')
+  // req.on('data', (chunk) => {
+  //   body += chunk
+  //   console.log(chunk)
+  // })
+
+  // if (method === 'POST' && url === '/echo') {
+  //   req.pipe(res)
+  // } else {
+  //   res.statusCode = 404
+  //   res.end('hello world')
+  // }
+
+  req.on('error', (err) => {
+    console.log(err)
   })
 
-  req.on('end', () => {
-    try {
-      const data = JSON.parse(body)
-      res.write(typeof data)
-      res.end()
-    } catch (err) {
-      res.statusCode = 400
-      return res.end(`error: ${ err.message }`)
-    }
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'X-Powered-By': 'bacon'
   })
+  res.end(`<html><body><h1>hello nodejs</h1></body></html>`)
+  // req.on('end', () => {
+  //   try {
+  //     res.writeHead(200, {
+  //       'Content-Type': 'application/json',
+  //       'X-Powered-By': 'bacon'
+  //     })
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.setHeader('X-Powered-By', 'bacon');
+  //     const data = JSON.parse(body)
+  //     let type = typeof data
+  //     res.end(`<html><body><h1>hello nodejs</h1></body></html>`)
+  //   } catch (err) {
+  //     res.statusCode = 400
+  //     return res.end(`error: ${ err.message }`)
+  //   }
+  // })
 })
 
 server.listen(3888)
